e07e7ea96c38344e1ea0de3b2d176ff90711f5a1
yongheng798 <1351817260@qq.com>
2020-06-03 20:12:03 +0800

    hello world

--- src/view/demo/Child.vue
+++ src/view/demo/Child.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="child">
-
     <h3>这里是子组件</h3>
-
-    <slot  :data="data"></slot>
+    <div>hello world</div>
+    <slot :data="data"></slot>
+    <h5>我来了了了了哈哈哈</h5>
   </div>
 </template>
 
@@ -27,12 +27,12 @@
 </script>
 
 <style scoped>
-  .child{
-    background-color: #00bbff;
-    width: 100%;
-    padding: 10px;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-  }
+.child {
+  background-color: #00bbff;
+  width: 100%;
+  padding: 10px;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
 </style>
